0d486a285c2f54744e0018a8ebf9e6edbead12fd
EXL <exlmotodev@gmail.com>
2022-05-16 16:50:06 +0700

    Fix form in Qt-TimeProblem demonstration
    Add ReadMe.md file and screenshot

---
+++ Examples/Qt-TimeProblem/ReadMe.md
@@ -0,0 +1,6 @@
+Time Problem
+============
+
+A demonstration the 2038 year problem using Qt framework.
+
+![Qt Time Problem](screen.png)
--- Examples/Qt-TimeProblem/TimeDialog.cpp
+++ Examples/Qt-TimeProblem/TimeDialog.cpp
@@ -13,7 +13,8 @@ TimeDialog::TimeDialog(QWidget *parent) :
 
     tableModel = new TimeTableModel();
     ui->tableView->setModel(tableModel);
-    ui->tableView->setColumnWidth(1, 250);
+    ui->tableView->setColumnWidth(0, 150);
+    ui->tableView->setColumnWidth(1, 300);
 
     setWindowTitle(tr("2038 problem"));
 }
